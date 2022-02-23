732005ad3112c854c61f50bf135fafc879130a59
Kr1st1an-F <100246316+Kr1st1an-F@users.noreply.github.com>
2022-02-23 20:42:21 +0100

    Add files via upload

---
+++ Functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+void convert (std::string& small);
+std::string namn_fel(std::string& namn);
+std::string city_fel(std::string stad);
+int menu();
+
+
+#endif
